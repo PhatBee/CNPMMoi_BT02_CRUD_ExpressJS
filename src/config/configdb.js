885ab0f5a07a9834b66793f88f3b2c2430199320
PhatBee <vinhphatst1235@gmail.com>
2025-08-20 11:13:32 +0700

    Fix

--- src/config/configdb.js
+++ src/config/configdb.js
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize';
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
-const sequelize = new Sequelize('node_fulltask', 'root', '1234567@a$', {
+const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: '127.0.0.1',
   dialect: 'mysql',
   logging: false
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from  './route/web';
-import connectDB from '../config/configdb';
+import connectDB from './config/configdb';
 
 require('dotenv').config();
 
